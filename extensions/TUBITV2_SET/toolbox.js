6b9b6c880a7308602a3b0120940c789487471f85
Dongye <132203726+Laishixun0612@users.noreply.github.com>
2025-11-25 18:09:16 +0800

    import external resources

---
+++ extensions/TUBITV2_SET/toolbox.js
@@ -0,0 +1,13 @@
+/* eslint-disable func-style */
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+function registerToolboxs () {
+    return `
+<category name="TUBIT V2" id="TUBITV2_Set_CATEGORY" colour="#ffbf00" secondaryColour="#ffbf00">
+    <block type="tubitv2_set" id="tubitv2_set">
+    </block>
+
+</category>`;
+}
+
+exports = registerToolboxs;
